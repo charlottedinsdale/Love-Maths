be851586fe1c1d7d033cf0f1963f77a8fd021e45
Charlotte Dinsdale <charlottedins01@gmail.com>
2024-11-06 11:27:55 +0000

    update subraction question function so operand 1 is always larger than operand 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,8 +98,8 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 function displaySubtractionQuestion(operand1, operand2) {
-    document.getElementById('operand1').textContent = operand1;
-    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById('operator').textContent = "-";
 }
 
